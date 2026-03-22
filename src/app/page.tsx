95cdc52d6e6221005df2f518ad93045ca6c0fc9f
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-23 00:28:38 +0900

    トップページの表示(ロゴとナビを左右に配置)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import styles from './page.module.css'
+
 export default function Home(){
   return (
-    <div>Hello World</div>
+    <div>
+      <header className={styles.header}>
+        <div className={styles.headerInner}>
+          <p>スケジュール管理APP</p>
+          <nav>
+            <ul className={styles.navList}>
+              <li>利用説明</li>
+              <li>ログイン</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </div>
   )
 }
