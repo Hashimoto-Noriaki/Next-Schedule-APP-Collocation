aa3f05a90c2b9bf27ea605a9d8c736f83b6ba219
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-23 00:51:05 +0900

    スタイルを完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import styles from './page.module.css'
 
 export default function Home(){
   return (
-    <div>
+    <div className={styles.wrapper}>
       <header className={styles.header}>
         <div className={styles.headerInner}>
-          <p>スケジュール管理APP</p>
+          <p className="logo">スケジュール管理APP</p>
           <nav>
             <ul className={styles.navList}>
               <li>利用説明</li>
